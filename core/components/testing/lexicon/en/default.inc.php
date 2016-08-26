2b8dcb5ad4c4a6025cba1d5a9f6dd755891430b1
OptimusCrime <thomasgautv@hotmail.com>
2016-08-26 22:58:39 +0200

    Add functionality to update a category

--- core/components/testing/lexicon/en/default.inc.php
+++ core/components/testing/lexicon/en/default.inc.php
@@ -3,6 +3,7 @@ $_lang['testing'] = 'Testing';
 $_lang['testing.name'] = 'Testing';
 $_lang['testing.desc'] = 'Description';
 $_lang['testing.category_create'] = 'Create new category';
+$_lang['testing.category_update'] = 'Edit category';
 $_lang['testing.parent'] = 'Parent category';
 $_lang['testing.name'] = 'Category name';
 $_lang['testing.no_parents'] = 'No parents';
